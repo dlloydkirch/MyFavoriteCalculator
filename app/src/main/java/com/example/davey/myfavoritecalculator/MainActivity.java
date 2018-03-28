64bbb19d6c3ef5af82af1ea55a0fcb86382a5edd
David Kirch <dlloydkirch@gmail.com>
2018-03-28 17:05:14 -0500

    Working on MainActivity

--- app/src/main/java/com/example/davey/myfavoritecalculator/MainActivity.java
+++ app/src/main/java/com/example/davey/myfavoritecalculator/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.davey.myfavoritecalculator;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
     }
 }
